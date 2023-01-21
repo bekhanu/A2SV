8d81f3a2e919ca429e13b146940a25c6ca32d645
bekhanu <101591823+bekhanu@users.noreply.github.com>
2023-01-21 11:10:43 +0300

    Create Selection sort.py

---
+++ 2089-find-target-indices-after-sorting-array/Selection sort.py
@@ -0,0 +1,11 @@
+
+class Solution: 
+    # def select(self, arr, i):
+    #     # code here 
+    
+    def selectionSort(self, arr,n):
+        #code here
+        for n in range(len(arr)):
+            arr.sort()
+
+        return arr
